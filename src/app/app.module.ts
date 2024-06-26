90ae81edcdf46a21283c9a2770564d48f2ff5717
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-06-26 12:54:11 -0600

    feat: initial project design and structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GifsModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
