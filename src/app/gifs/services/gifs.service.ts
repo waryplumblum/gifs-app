574a6d463b25848275fc91bdf0ccc755e2a56b94
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-06-27 14:07:13 -0600

    feat: read LocalStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,7 +13,9 @@ export class GifsService {
   private _tagHistory: string[] = [];
   public  gifList:     Gif   [] = [];
 
-  constructor( private http:HttpClient ) { }
+  constructor( private http:HttpClient ) {
+    this.loadLocalStorage();
+  }
 
   get tagsHistory() {
     return [...this._tagHistory];
@@ -29,6 +31,13 @@ export class GifsService {
     this._tagHistory.unshift( tag );
     this._tagHistory = this.tagsHistory.splice(0,10);
     this.saveLocalStorage();
+  }
+
+  private loadLocalStorage():void{
+    if( !localStorage.getItem('history')) return;
+    this._tagHistory = JSON.parse( localStorage.getItem('history')! );
+    if( this._tagHistory.length === 0) return;
+    this.searchTag( this._tagHistory[0]);
   }
 
   private saveLocalStorage():void{
