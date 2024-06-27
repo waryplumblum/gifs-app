16690bc75135a3144a4e9d88228899d1eefe97c4
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-06-27 10:56:33 -0600

    feat: validate gifsService

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,8 +13,20 @@ export class GifsService {
     return [...this._tagHistory];
   }
 
-  searchTag( tag:string ):void {
+  private organizeHistory( tag:string ) {
+    tag = tag.toLowerCase();
+
+    if( this._tagHistory.includes( tag )) {
+      this._tagHistory = this._tagHistory.filter( (oldTag) => oldTag !== tag);
+    }
+
     this._tagHistory.unshift( tag );
+    this._tagHistory = this.tagsHistory.splice(0,10);
+  }
+
+  searchTag( tag:string ):void {
+    if ( tag.length ===0 ) return;
+    this.organizeHistory( tag );
   }
 
 }
